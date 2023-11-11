57be7805ededc91a98fe9c0b4bc0cee6833b983a
Christopher Vaughn <vaughnc@yosemite.edu>
2023-11-11 11:47:45 -0800

    done

--- Functions.cpp
+++ Functions.cpp
@@ -26,17 +26,42 @@ int change_ref(int& c, int& d)
 	return c + d;
 }
 
+// Function declaration
+//takes a default value
+//if no value is passed, default value is used
+//default values are always at the end
+int change_def(int c, int d = 10)
+{
+	return c + d;
+}
 
+// Function declaration
+//overloading
+//same function name with different parameters
+//return type is not considered
+int change_def(int c, double d = 10)
+{
+	return c + d;
+}
 
 // Main function
 int main()
 {
 	int a = 10, b=20;
+	double c = 10.2;
+	//Print the value returned by the function
 	cout << return_something() << endl;
+	//Print the value returned by the function
 	cout << change_cpy(a, b) << endl;
+	//Print the value of a and b after the function call
 	cout << a << " " << b << endl;
+	//Print the value returned by the function
 	cout << change_ref(a, b) << endl;
+	//Print the value of a and b after the function call
 	cout << a << " " << b << endl;
+	//Print the value returned by the function
+	//Default value is used for d
+	cout << change_def(a, c) << endl;
 
 	return 0;
 }
